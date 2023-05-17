094ffd4d7faf5d1e3e2b35f3f3c44e9c31c5ba6a
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 00:41:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-feather-d-83950",
+  url: "https://weathered-feather-d-83950.botics.co",
+  api: "https://weathered-feather-d-83950.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
